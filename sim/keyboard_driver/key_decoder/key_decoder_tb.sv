2074ef68800515b53adef8e44d1072fc9866f1f6
KacperS <ks.sokolowski@onet.pl>
2026-05-13 21:28:47 +0200

    feat: key_decoder testbench

--- sim/keyboard_driver/key_decoder/key_decoder_tb.sv
+++ sim/keyboard_driver/key_decoder/key_decoder_tb.sv
@@ -9,7 +9,7 @@ module key_decoder_tb;
      *  Local parameters
      */
 
-    localparam CLK_PERIOD = 15.3846;    //65MHz
+    localparam CLK_PERIOD = 10.0;  //100MHz
     localparam RST_START_TIME = 1.25 * CLK_PERIOD;
     localparam RST_ACTIVE_TIME = 2.00 * CLK_PERIOD;
     
@@ -18,6 +18,67 @@ module key_decoder_tb;
      */
     
     
-     logic clk, rst_n, ps2_clk, ps2_data;
-     logic [7:0] scan_code;
+    logic clk, rst_n, done;
+    logic [7:0] scan_code;
+    logic up, down, left, right, space, tab;
+
+    key_decoder dut(
+        .clk(clk),
+        .rst_n(rst_n),
+        .key_code(scan_code),
+        .done(done),
+        .up(up),
+        .down(down),
+        .left(left),
+        .right(right),
+        .space(space),
+        .tab(tab)
+    );
+
+    initial begin
+        clk = 0;
+        forever #(CLK_PERIOD/2) clk = ~clk;
+    end
+
+    task drive_code (input [7:0] code);
+        begin
+            @(posedge clk);
+            scan_code = code;
+            done = 1;
+            @(posedge clk)
+            done = 0;
+            repeat(20) @(posedge clk);
+        end
+    endtask
+
+    initial begin
+
+        rst_n = 0;
+        done = 0;
+        scan_code = 0;
+        repeat(100) @(posedge clk);
+        rst_n = 1;
+
+        $display("Simulating Space Press (0x29)");
+        drive_code(8'h29);
+        repeat(10) @(posedge clk);
+        if (space) $display("Space signal is HIGH");
+
+        $display("Simulating Space release (0xF0, 0x29)");
+        drive_code(8'hf0);
+        drive_code(8'h29);
+        repeat(10) @(posedge clk);
+
+        if (!space) $display("Space signal is LOW");
+
+        $display("Simulating Up Arrow (0x75)");
+        drive_code(8'h75);
+        repeat(10) @(posedge clk);
+        if (up) $display("Up signal is HIGH");
+
+        repeat(100) @(posedge clk);
+        $finish;
+    end
+
+endmodule
     
