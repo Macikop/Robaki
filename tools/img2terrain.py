bccc315b9bfce00ba3652bb93a38f15c2801f4f4
Maciej <maciej.piofczyk@gmail.com>
2026-05-15 17:27:35 +0200

    feat: new test structure

--- tools/img2terrain.py
+++ tools/img2terrain.py
@@ -1,11 +1,7 @@
 import sys
 import re
-import math
 from PIL import Image
 import numpy as np
-
-def next_pow2(x):
-    return 1 << (x - 1).bit_length()
 
 # input image
 image_file = sys.argv[1]
@@ -14,39 +10,30 @@ image_file = sys.argv[1]
 image = Image.open(image_file).convert("L")
 array = np.array(image)
 
-orig_w = image.width
-orig_h = image.height
-
-pad_w = next_pow2(orig_w)
-pad_h = next_pow2(orig_h)
+width = image.width
+height = image.height
 
 # output file
 output_file_name = re.sub(r'\.[a-zA-Z0-9]+$', '.dat', image_file)
-output_file = open(output_file_name, 'w')
 
-output_file.write("// 1-bit monochrome image ROM\n")
-output_file.write("// original WIDTH = {}\n".format(orig_w))
-output_file.write("// original HEIGHT = {}\n".format(orig_h))
-output_file.write("// PAD WIDTH = {}\n".format(pad_w))
-output_file.write("// PAD HEIGHT = {}\n".format(pad_h))
-output_file.write("// BIT = 0 white, 1 black\n")
-
-# threshold (you can tune this)
+# threshold
 THRESHOLD = 128
 
-for y in range(pad_h):
-    for x in range(pad_w):
+with open(output_file_name, 'w') as output_file:
 
-        if y < orig_h and x < orig_w:
+    output_file.write("// 1-bit monochrome image ROM\n")
+    output_file.write(f"// WIDTH = {width}\n")
+    output_file.write(f"// HEIGHT = {height}\n")
+    output_file.write("// BIT = 0 white, 1 black\n")
+
+    for y in range(height):
+        for x in range(width):
+
             pixel = array[y, x]
-            bit = 0 if pixel > THRESHOLD else 1  # white=0, black=1
-        else:
-            bit = 0  # padding = white
 
-        # store as hex (4 pixels per hex digit if you want later packing,
-        # but here we keep 1 bit per line like your original)
-        output_file.write(str(bit) + "\n")
+            # white = 0, black = 1
+            bit = 0 if pixel > THRESHOLD else 1
 
-output_file.close()
+            output_file.write(f"{bit}\n")
 
 print("Saved:", output_file_name)
