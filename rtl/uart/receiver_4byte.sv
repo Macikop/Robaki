535c8d9f77769f93562b3d317a510d2e0c93e97a
KacperS <ks.sokolowski@onet.pl>
2026-06-13 19:13:06 +0200

    uart: 4 byte receiver module complete with tb

--- rtl/uart/receiver_4byte.sv
+++ rtl/uart/receiver_4byte.sv
@@ -13,7 +13,8 @@ timeprecision 1ps;
 
 logic [31:0] data;
 logic filled;
-logic [3:0] byte_counter;
+logic [1:0] byte_counter;
+
 assign full = filled;
 assign data32 = data;
 
@@ -21,18 +22,17 @@ always_ff @(posedge clk or negedge rst_n) begin
     if(!rst_n) begin
         data <= '0;
         filled <= '0;
-        byte_counter <= 4'b1000;
+        byte_counter <= '0;
     end else begin
-        if(!rx_empty && !filled && rd_uart) begin
-            data[7:0] <= r_data;
-            data <= data << 8;
-            byte_counter <= {byte_counter[0], byte_counter[3:1]};
-        end
+        if(!rx_empty && rd_uart) begin
+            data <= {r_data, data32[31:8]};
+            byte_counter <= byte_counter + 1;
 
-        if(byte_counter == 4'b1000) begin
-            filled <= 1'b1;
-        end else begin
-            filled <= 1'b0;
+            if(byte_counter == 3'd3) begin
+                filled <= 1'b1;
+            end else begin
+                filled <= 1'b0;
+            end
         end
     end
 end
