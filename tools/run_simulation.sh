ffdf52a273d5a0c4e602e3041dddb5892ad76bb7
Kacper Sokolowski <ks.sokolowski@onet.pl>
2026-07-06 12:05:01 +0200

    Update run_simulation.sh

--- tools/run_simulation.sh
+++ tools/run_simulation.sh
@@ -4,6 +4,8 @@
 # MTM UEC2
 # Author: Piotr Kaczmarczyk
 #
+# Modified by MP to accept test_dir inside directory
+#
 # Description:
 # This script runs simulations outside Vivado, making them faster.
 # For usage details run the script with no arguments.
@@ -19,10 +21,11 @@
 function usage {
     echo "usage: $(basename "$0") [options]"
     echo "  options:"
-    echo "    -l         list available tests"
-    echo "    -t <test>  run the specified <test>"
-    echo "    -g         show gui (use with -t)"
-    echo "    -a         run all available tests (does not work with gui)"
+    echo "    -l          list available tests"
+    echo "    -t <test>   run the specified <test>"
+    echo "    -g          show gui (use with -t)"
+    echo "    -a          run all available tests (does not work with gui)"
+    echo "    -d <dir>    run all tests inside a specific directory section"
     exit 1
 }
 
@@ -42,7 +45,6 @@ function execute_test {
     test_name=$(basename "$test_path")
     PRJ_FILE=${ROOT_DIR}/sim/${test_path}/${test_name}.prj
 
-    # Elaboration and simulation options
     if [[ $(grep 'glbl.v' -oc  ${PRJ_FILE}) -gt 0 ]]; then
         COMPILE_GLBL='work.glbl'
     else
@@ -68,9 +70,19 @@ function execute_test {
     cd ..
 }
 
-# Run all available simulations
 function run_all {
-    for test in $(list_available_tests); do
+    # If target_dir is provided, limit search scope to that directory
+    local search_path="."
+    if [[ -n "$1" ]]; then
+        search_path="./$1"
+        if [[ ! -d "$search_path" ]]; then
+            echo -e "\033[1;31mError: Directory 'sim/$1' does not exist.\033[0;39m"
+            exit 1
+        fi
+        echo -e "\033[1;34m=== Running Simulations in Section: $1 ===\033[0;39m"
+    fi
+
+    for test in $(find "$search_path" -type f -name "*.prj" | sed 's|^\./||' | sed 's|/[^/]*$||'); do
         err_ctr=0
         echo -en "${test}:\t"
         err_ctr=$(execute_test ${test} | grep -oic 'error')
@@ -83,10 +95,6 @@ function run_all {
     exit 0
 }
 
-# ------------------------------------------------------------------------------
-# Arguments parsing and checking
-# ------------------------------------------------------------------------------
-
 if [[ $# -eq 0 ]]; then
     usage
 fi
@@ -94,15 +102,22 @@ fi
 ROOT_DIR=$(pwd)
 cd sim
 
-while getopts aglrs:t: option; do
+# Added 'd:' to getopts string to accept the directory argument
+while getopts aglrs:t:d: option; do
     case ${option} in
         g) show_gui=1;;
         l) list_available_tests;;
         t) test_name=${OPTARG};;
         a) run_all;;
+        d) target_dir=${OPTARG};;
         *) usage;;
     esac
 done
+
+# If a specific directory section was requested, run all tests inside it
+if [[ ${target_dir} ]]; then
+    run_all "${target_dir}"
+fi
 
 if [[ ${test_name} ]]; then
     execute_test ${test_name}
